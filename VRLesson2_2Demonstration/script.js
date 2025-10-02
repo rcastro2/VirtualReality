e4dbb6949919ae318e16d000a39dda9b1f8db807
Renne Castro <rcastro2@users.noreply.github.com>
2025-10-02 07:28:08 -0400

    adds Lesson 2.2 Demonstration

---
+++ VRLesson2_2Demonstration/script.js
@@ -0,0 +1,35 @@
+let scene;
+function rnd(l, u){
+  return Math.floor(Math.random()*(u-l) + l);
+}
+
+window.addEventListener("DOMContentLoaded",function() {
+  scene = document.querySelector("a-scene"); //CSS Selector
+
+  for(let i = 0; i < 100; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    createTree(x, 0 , z);
+  }
+})
+
+function createTree(x, y, z){
+  let tree = document.createElement("a-entity");
+  
+  let pines = document.createElement("a-cone");
+  pines.setAttribute("color","green");
+  pines.setAttribute("position","0 2 0");
+  pines.setAttribute("height","2");
+  tree.append( pines );
+
+  let stump = document.createElement("a-cylinder");
+  stump.setAttribute("position","0 1 0");
+  stump.setAttribute("color","brown");
+  stump.setAttribute("radius","0.25");
+  tree.append( stump );
+
+  tree.setAttribute("position",{x:x, y:y, z:z});
+  scene.append( tree )
+}
+
+
